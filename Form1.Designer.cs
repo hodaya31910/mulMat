63359b399b2269be7d2d1f6b731a8676a167f146
hodaya <hodaya31910@gmail.com>
2021-11-15 23:02:07 +0200

    first comit

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace mulMat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(352, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "הכנס גודל מטריצה";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.SystemColors.Info;
+            this.numericUpDown1.Location = new System.Drawing.Point(380, 112);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(180, 26);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Thistle;
+            this.button1.Location = new System.Drawing.Point(81, 163);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 66);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "הכפלה סידרתית";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Thistle;
+            this.button2.Location = new System.Drawing.Point(518, 163);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(342, 66);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "הכפלה מקבילית";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(921, 755);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mulMat
+{
+  
+    public partial class Form1 : Form
+    {
+        int size;
+        int[,] mat1;
+        int[,] mat2;
+        int[,] mat3;
+        Label l2 = new Label();
+        Label l1 = new Label();
+        Label l3 = new Label();
+
+        public Form1()
+        {
+            InitializeComponent();
+            l3.ForeColor = Color.Red;
+            this.AutoSize = false;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Label l2 = new Label();
+            Label l1 = new Label();
+            Label l3 = new Label();
+            l1.ForeColor = Color.Thistle;
+            l2.ForeColor = Color.Thistle;
+
+            size = Convert.ToInt32(numericUpDown1.Value);
+            mat1 = new int[size, size];
+            mat2 = new int[size, size];
+            mat3 = new int[size, size];
+            l3.ForeColor = Color.Red;
+            Random r = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    mat1[i, j] = r.Next(1, 10);
+
+                    l1.Text += mat1[i, j].ToString() + "   ";
+                    Point p = new Point(100, 200);
+                    l1.Location = p;
+                    l1.Width = 100;
+                    l1.Height = 100;
+                    this.Controls.Add(l1);
+
+                    mat2[i, j] = r.Next(1, 10);
+                    Point p2 = new Point(200, 200);
+                    l2.Text += mat2[i, j].ToString() + "   ";
+                    l2.Location = p2;
+                    l2.Width = 100;
+                    l2.Height = 100;
+                    this.Controls.Add(l2);
+                }
+                l1.Text += "\n";
+                l2.Text += "\n";
+            }
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            sidarty();
+            watch.Stop();
+            MessageBox.Show(watch.Elapsed.ToString());
+        }
+    
+
+        private void sidarty()
+        {
+            l3 = new Label();
+            for (int i = 0; i < size; i++)
+            {
+
+                for (int j = 0; j < size; j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < size; k++)
+                        sum += mat1[i, k] * mat2[k, j];
+                    mat3[i, j] = sum;
+                    Point p3 = new Point(150, 300);
+                    l3.Text += mat3[i, j].ToString() + "   ";
+                    l3.Location = p3;
+                    l3.Width = 200;
+                    l3.Height = 200;
+                    this.Controls.Add(l3);
+                }
+                l3.Text += "\n";
+            }
+        }
+
+        private void parallel()
+        {
+            l3 = new Label();
+            l3.ForeColor = Color.Red;
+
+            Thread[,] thread = new Thread[size, size];
+            for (int i = 0; i < size; i++)
+            {
+
+                for (int j = 0; j < size; j++)
+                {
+                    int sum = 0;
+                    Element e = new Element();
+                    e.x = i;
+                    e.y = j;
+                    thread[i, j] = new Thread(new ParameterizedThreadStart(ComputeElements));
+                    thread[i, j].Start(e);
+                }
+                for (int l = 0; l < size; l++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        thread[i,j].Join();
+                    }
+                    
+                }
+        }
+            for (int l = 0; l < size; l++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Point p3 = new Point(400, 300);
+                    l3.Text += mat3[l, j].ToString() + "   ";
+                    l3.Location = p3;
+                    l3.Width = 200;
+                    l3.Height = 200;
+                    this.Controls.Add(l3);
+                }
+                l3.Text += "\n";
+            }
+
+        }
+
+        private void ComputeElements(object obj)
+        {
+            l3.ForeColor = Color.Red;
+            Label l2 = new Label();
+            Label l1 = new Label();
+            Element e = (Element)obj;
+            int sum = 0;
+            for (int k = 0; k < size; k++)
+                sum += mat1[e.x, k] * mat2[k, e.y];
+            mat3[e.x, e.y] = sum;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            l3.ForeColor = Color.Red;
+            l1 = new Label();
+            l2= new Label();
+            l1.AutoSize= false;
+            l1.Height = 40;
+            l1.Width = 30;
+            l1.ForeColor = Color.Thistle;
+            l2.ForeColor = Color.Thistle;
+            size = Convert.ToInt32(numericUpDown1.Value);
+            mat1 = new int[size, size];
+            mat2 = new int[size, size];
+            mat3 = new int[size, size];
+            Random r = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    mat1[i, j] = r.Next(1, 10);
+
+                    l1.Text += mat1[i, j].ToString() + "   ";
+                    Point p = new Point(350, 200);
+                    l1.Location = p;
+                    l1.Width = 100;
+                    l1.Height = 100;
+                    this.Controls.Add(l1);
+
+                    mat2[i, j] = r.Next(1, 10);
+                    Point p2 = new Point(450, 200);
+                    l2.Text += mat2[i, j].ToString() + "   ";
+                    l2.Location = p2;
+                    l2.Width = 100;
+                    l2.Height = 100;
+                    this.Controls.Add(l2);
+                }
+                l1.Text += "\n";
+                l2.Text += "\n";
+            }
+            Stopwatch watch2 = new Stopwatch();
+            watch2.Start();
+            parallel();
+            watch2.Stop();
+            MessageBox.Show(watch2.Elapsed.ToString());
+            
+        }
+    }
+    }
+
